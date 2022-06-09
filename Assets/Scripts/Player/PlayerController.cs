1a84e9a2cd88d894387e920d7dc64fcfc07c1074
Mauricio Rodriguez Becerra <mrodriguezbec@unal.edu.co>
2022-06-09 14:53:22 -0500

    Initial commit
    First Commit For Project

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    // 
+    [RequireComponent(typeof(Rigidbody2D))]
+    
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float playerSpeed = 5f;
+        
+        private Vector2 _playerMovement;
+        private Rigidbody2D _playerRigidbody;
+        
+        // 
+        private void Awake()
+        {
+            _playerRigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        // 
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _playerMovement = context.ReadValue<Vector2>();
+        }
+        
+        // Rigidbody Movement
+        public void FixedUpdate()
+        {
+            _playerRigidbody.velocity = _playerMovement * playerSpeed * Time.fixedDeltaTime;
+        }
+        
+    }
+}
